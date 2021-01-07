b23c712151dc4ce1d3bcafad4de18406bc69fb9d
BowHanna21 <amersarangani22@gmail.com>
2021-01-08 03:16:46 +0800

    shit

--- www/itrapi.php
+++ www/itrapi.php
@@ -14,10 +14,10 @@
 		exit(0);
 	}
 
-	define('DB_NAME', 'No0gC5dY8q');
-	define('DB_USER', 'No0gC5dY8q');
-	define('DB_PASSWORD', 'U7ukpHfCbw');
-	define('DB_HOST', 'remotemysql.com');
+	define('DB_NAME', 'sql12375138');
+	define('DB_USER', 'sql12375138');
+	define('DB_PASSWORD', 'trDm7SrT91');
+	define('DB_HOST', 'sql12.freesqldatabase.com');
 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	
@@ -28,7 +28,7 @@
 
 		foreach ($request as $item) {
 
-			$sqltemp_itr = mysqli_query($mysqli, "INSERT INTO temp_itr(family_serial_no,age,mode_transaction,date_consultation,time_consultation,blood_pressure,temperature,height,weight,name_of_attending,nature_of_visit,chief_complaints,diagnosis,medication,lab_findings,name_health_careprovider,performed_lab_test,referred_from,referred_to,reason_of_referral,referred_by,added_by,submitted_by,patient_id,date_submitted) VALUES ('".$item['family_serial_no']."','".$item['age']."','".$item['mode_transaction']."','".$item['date_consultation']."','".$item['time_consultation']."','".$item['blood_pressure']."','".$item['temperature']."','".$item['height']."','".$item['weight']."','".$item['name_of_attending']."','".$item['nature_of_visit']."','".$item['chief_complaints']."','".$item['diagnosis']."','".$item['medication']."','".$item['lab_findings']."','".$item['name_health_careprovider']."','".$item['performed_lab_test']."','".$item['referred_from']."','".$item['referred_to']."','".$item['reason_of_referral']."','".$item['referred_by']."','".$item['added_by']."','".$item['submitted_by']."','".$item['patient_id']."','".$item['date_submitted']."')");
+			$sqltemp_itr = mysqli_query($mysqli, "INSERT INTO temp_itr(family_serial_no,age,mode_transaction,date_consultation,time_consultation,blood_pressure,temperature,height,weight,name_of_attending,nature_of_visit,chief_complaints,diagnosis,medication,lab_findings,name_health_careprovider,performed_lab_test,referred_from,referred_to,reason_of_referral,referred_by,added_by,submitted_by,patient_id,date_submitted) VALUES ('".$item[family_serial_no]."','".$item[age]."','".$item[mode_transaction]."','".$item[date_consultation]."','".$item[time_consultation]."','".$item[blood_pressure]."','".$item[temperature]."','".$item[height]."','".$item[weight]."','".$item[name_of_attending]."','".$item[nature_of_visit]."','".$item[chief_complaints]."','".$item[diagnosis]."','".$item[medication]."','".$item[lab_findings]."','".$item[name_health_careprovider]."','".$item[performed_lab_test]."','".$item[referred_from]."','".$item[referred_to]."','".$item[reason_of_referral]."','".$item[referred_by]."','".$item[added_by]."','".$item[submitted_by]."','".$item[patient_id]."','".$item[date_submitted]."')");
 		}
 
 		if(!$sqltemp_itr){
