758fa53be48c7db0fdb0fef651e24190bc356615
BowHanna21 <amersarangani22@gmail.com>
2021-01-12 22:32:10 +0800

    try upload

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,7 @@ $con = new mysqli($localhost, $username, $password, $dbname);
 
 // check connection
 if($con->connect_error) {
-	die("connection failed2 : " . $connect->connect_error);
+	die("connection failed : " . $connect->connect_error);
 } else {
 	//echo "Successfully Connected";
 }
