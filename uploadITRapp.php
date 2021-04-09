9a55fe2ac913bdf853a9d723860ab19b76189783
BowHanna21 <amersarangani22@gmail.com>
2021-04-09 22:34:30 +0800

    UPDATE!!

--- uploadITRapp.php
+++ uploadITRapp.php
@@ -107,7 +107,7 @@ if (isset($_POST['deletebutton']))
         die(); 
     }
 
-    $userid = $_GET['userid'];
+    $userid = $_SESSION['userid'];
     $sql = "SELECT fname, lname from acc_info where ai_id=$userid";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result);
@@ -220,7 +220,7 @@ if (isset($_POST['deletebutton']))
     <div class="container-fluid expanded-panel">
         <div class="row">
             <div id="logo" class="col-xs-12 col-sm-2">
-                <a href="homeOIC.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">MCHOIMS</a>
+                <a href="homeOIC.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">MCHOIMS</a>
             </div>
             <div id="top-panel" class="col-xs-12 col-sm-10">
                 <div class="row">
@@ -237,7 +237,7 @@ if (isset($_POST['deletebutton']))
                                     <i class="fa fa-angle-down pull-right"></i>
                                     <div class="user-mini pull-right">
                                         <span class="welcome">Welcome,</span>
-                                        <span><?php $id=$_GET['userid'];
+                                        <span><?php $id=$_SESSION['userid'];
                                         $sql = "SELECT fname, lname from acc_info where ai_id=$id";
                                         $result = mysqli_query($con,$sql);
                                         $row = mysqli_fetch_array($result);
@@ -254,7 +254,7 @@ if (isset($_POST['deletebutton']))
                                 <ul class="dropdown-menu">
                                     
                                     <li>
-                                        <a href="profileofficer.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">
+                                        <a href="profileofficer.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">
                                             <i class="fa fa-user"></i>
                                             <span class="hidden-sm text">Profile</span>
                                         </a>
@@ -280,7 +280,7 @@ if (isset($_POST['deletebutton']))
         <div id="sidebar-left" class="col-xs-2 col-sm-2">
             <ul class="nav main-menu">
                 <li>
-                    <a href="homeOIC.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">
+                    <a href="homeOIC.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">
                         <i class="fa fa-dashboard"></i>
                         <span class="hidden-xs">Home</span>
                     </a>
@@ -292,8 +292,8 @@ if (isset($_POST['deletebutton']))
                         <span class="hidden-xs">Activities/Programs</span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a href="updateactivity.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Update Activities/Programs</a></li>
-                        <li><a href="addactivity.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Add Activities/Programs</a></li>
+                        <li><a href="updateactivity.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Update Activities/Programs</a></li>
+                        <li><a href="addactivity.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Add Activities/Programs</a></li>
 
                     </ul>
                 </li>
@@ -309,9 +309,9 @@ if (isset($_POST['deletebutton']))
                                 <span class="hidden-xs">Patient Enrollment Records</span>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="viewPER.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">List of Records</a></li>
-                                <li><a href="updatePER.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Update Records</a></li>
-                                <li><a href="addPER.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Add Record</a></li>
+                                <li><a href="viewPER.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">List of Records</a></li>
+                                <li><a href="updatePER.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Update Records</a></li>
+                                <li><a href="addPER.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Add Record</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
@@ -320,8 +320,8 @@ if (isset($_POST['deletebutton']))
                                 <span class="hidden-xs">Individual Treatment Record</span>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="viewITR.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">List of Records</a></li>
-                                <li><a href="updateITR.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Update Records</a></li>
+                                <li><a href="viewITR.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">List of Records</a></li>
+                                <li><a href="updateITR.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Update Records</a></li>
                             </ul>
                         </li>
                     </ul>               
@@ -345,7 +345,7 @@ if (isset($_POST['deletebutton']))
                                ?>   
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a href="uploadPER.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">PER from User<?php
+                        <li><a href="uploadPER.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">PER from User<?php
                                 $sql="SELECT count(*) as cntupload from temp_per where added_by = 'user'";
                                 $result = mysqli_query($con,$sql);
                                $row = mysqli_fetch_array($result);
@@ -355,7 +355,7 @@ if (isset($_POST['deletebutton']))
                                 echo "<span class='badge'>". $count ."</span>";
                                }
                                ?></a></li>
-                        <li><a href="uploadITR.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">ITR from User<?php
+                        <li><a href="uploadITR.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">ITR from User<?php
                                 $sql="SELECT count(*) as cntupload from temp_itr where added_by = 'user'";
                                 $result = mysqli_query($con,$sql);
                                $row = mysqli_fetch_array($result);
@@ -385,7 +385,7 @@ if (isset($_POST['deletebutton']))
                                 echo "<span class='badge'>". $count ."</span>";
                                }
                                ?></a></li>                
-                        <li><a href="uploadPERbrgy.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">PER from Barangay<?php
+                        <li><a href="uploadPERbrgy.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">PER from Barangay<?php
                                 $sql="SELECT count(*) as cntupload from temp_per where added_by='brgy'";
                                 $result = mysqli_query($con,$sql);
                                $row = mysqli_fetch_array($result);
@@ -395,7 +395,7 @@ if (isset($_POST['deletebutton']))
                                 echo "<span class='badge'>". $count ."</span>";
                                }
                                ?></a></li>
-                        <li><a href="uploadITRbrgy.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">ITR from Barangay<?php
+                        <li><a href="uploadITRbrgy.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">ITR from Barangay<?php
                                 $sql="SELECT count(*) as cntupload from temp_itr where added_by='brgy'";
                                 $result = mysqli_query($con,$sql);
                                $row = mysqli_fetch_array($result);
@@ -421,7 +421,7 @@ if (isset($_POST['deletebutton']))
             <div class="row">
                 <div id="breadcrumb" class="col-md-12">
                     <ol class="breadcrumb">
-                        <li><a href="homeOIC.php?userid=<?php $id=$_GET['userid']; echo $id; ?>">Home</a></li>
+                        <li><a href="homeOIC.php?userid=<?php $id=$_SESSION['userid']; echo $id; ?>">Home</a></li>
                         <li><a href="#">Uploads</a></li>
                         <li><a href="#">ITR</a></li>
                     </ol>
