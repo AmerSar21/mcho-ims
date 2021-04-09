097e57cc076526a0f0ca4d082e75442b62fa5713
BowHanna21 <amersarangani22@gmail.com>
2021-04-09 20:38:55 +0800

    UPDATE!!

--- uploadITRapp.php
+++ uploadITRapp.php
@@ -361,30 +361,6 @@ if(isset($_POST['acceptbutton']))
                         </ol>
                     </div>
                 </div>
-                <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <form role="form" method="post">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                <h4 class="modal-title" id="myModalLabel">Delete user</h4>
-                            </div>
-                            <div class="modal-body">
-                                <p>Are you sure you want to delete this Record?</p>
-                                <p class="text-danger">This action cannot be undone.</p>
-                                <input class="form-control" type="hidden" name="iddelete" id="m_iddelete" >
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                                <button type="submit" name="deletebutton" class="btn btn-danger">Delete</button>
-                            </div>
-                        
-                        </form>
-                        <!-- /.modal-content -->
-                        </div>
-                    </div>
-                    <!-- /.modal-dialog -->
-                </div>
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="box">
