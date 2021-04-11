b39b1d88d67f6c334352344c1e377b62c35e8518
BowHanna21 <amersarangani22@gmail.com>
2021-04-11 21:04:39 +0800

    Update

--- uploadPERcsv.php
+++ uploadPERcsv.php
@@ -1,18 +1,19 @@
 <?php
 
 include("db_connect.php");
+session_start();
 
- $userid = $_GET['userid'];
-    $sql = "SELECT fname, lname from acc_info where ai_id=$userid";
-    $result = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($result);
-    $submittedby =$row['fname']." ".$row['lname'];
- $row=0;
+$userid = $_SESSION['userid'];
+$sql = "SELECT fname, lname from acc_info where ai_id=$userid";
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result);
+$submittedby =$row['fname']." ".$row['lname'];
+$row=0;
+
  if(isset($_POST["Import"])){
         
         $filename=$_FILES["file"]["tmp_name"];      
- 
- 
+
          if($_FILES["file"]["size"] > 0)
          {
             $file = fopen($filename, "r");
@@ -26,6 +27,9 @@ include("db_connect.php");
                $sql = "INSERT into temp_per (`family_serial_no`, `lname`, `fname`, `mname`, `sex`, `b_date`, `b_place`, `bloodtype`, `civil_stat`, `spouse_name`, `mothers_name`, `fam_position`, `home_no`, `street`, `barangay`, `city`, `province`, `contact_no`, `educ_attainment`, `employ_status`, `ph_member`, `ph_no`, `member_category`, `facility_no`, `dswdnhts`, `suffix`, added_by, submitted_by, patient_id) 
                    values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."','".$getData[12]."','".$getData[13]."','".$getData[14]."','".$getData[15]."','".$getData[16]."','".$getData[17]."','".$getData[18]."','".$getData[19]."','".$getData[20]."','".$getData[21]."','".$getData[22]."','".$getData[23]."','".$getData[24]."','".$getData[25]."','user', '$submittedby','".$getData[26]."')";
                    $result = mysqli_query($con, $sql);
+                   var_dump($result);
+                   die();
+
                 if(!isset($result))
                 {
                     echo "<script type='text/javascript'>
