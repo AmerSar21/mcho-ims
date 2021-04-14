a18a4362c34f3acfaa54d4bfa4f0e0fb1c11319f
BowHanna21 <amersarangani22@gmail.com>
2021-04-15 01:00:25 +0800

    Update na please

--- uploadPERcsv.php
+++ uploadPERcsv.php
@@ -12,8 +12,8 @@ $row=0;
 
  if(isset($_POST["Import"])){
         
-        $filename=$_FILES["file"]["tmp_name"];      
-
+        $filename=$_FILES["file"]["tmp_name"];     
+        $date = date("Y/m/d");
         // echo "Filename: " . $_FILES['file']['name']."<br>";
         // echo "Type : " . $_FILES['file']['type'] ."<br>";
         // echo "Size : " . $_FILES['file']['size'] ."<br>";
@@ -31,15 +31,15 @@ $row=0;
                 if($row == 1) continue;
 
                 if ($getData[0]) {
-               $sql = "INSERT into temp_per (`family_serial_no`, `lname`, `fname`, `mname`, `sex`, `b_date`, `b_place`, `bloodtype`, `civil_stat`, `spouse_name`, `mothers_name`, `fam_position`, `home_no`, `street`, `barangay`, `city`, `province`, `contact_no`, `educ_attainment`, `employ_status`, `ph_member`, `ph_no`, `member_category`, `facility_no`, `dswdnhts`, `suffix`, added_by, submitted_by, patient_id) 
-                   values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."','".$getData[12]."','".$getData[13]."','".$getData[14]."','".$getData[15]."','".$getData[16]."','".$getData[17]."','".$getData[18]."','".$getData[19]."','".$getData[20]."','".$getData[21]."','".$getData[22]."','".$getData[23]."','".$getData[24]."','".$getData[25]."','user', '$submittedby','".$getData[26]."')";
+               $sql = "INSERT into temp_per (family_serial_no,lname,fname,mname,suffix,sex,b_place,b_date,bloodtype,civil_stat,spouse_name,educ_attainment,employ_status,fam_position,patient_id,home_no,street,barangay,city,province,mothers_name,contact_no,dswdnhts,facility_no,ph_member,ph_no,added_by,submitted_by,member_category,date_submitted) 
+                   values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."','".$getData[12]."','".$getData[13]."','".$getData[14]."','".$getData[15]."','".$getData[16]."','".$getData[17]."','".$getData[18]."','".$getData[19]."','".$getData[20]."','".$getData[21]."','".$getData[22]."','".$getData[23]."','".$getData[24]."','".$getData[25]."','user', '$submittedby','".$getData[26]."','$date')";
                    $result = mysqli_query($con, $sql);
                    // echo "sql: " . $sql . "</br";
                    // var_dump($result);
                    // die();
 
                 if(!isset($result))
-                {
+                { 
                     echo "<script type='text/javascript'>
                             alert('Invalid File:Please Upload CSV File.');                        
                             window.location.href = 'addPERuser.php?userid=".$userid."';
