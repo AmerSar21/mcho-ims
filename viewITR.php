0173d5b920b21ed19c97b7c524bfa108f9325df6
BowHanna21 <amersarangani22@gmail.com>
2021-04-16 23:50:07 +0800

    Update userperpage

--- viewITR.php
+++ viewITR.php
@@ -362,14 +362,6 @@ session_start();
 <script src="js/devoops.js"></script>
 <script src="../vendor/jquery/jquery.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
 </body>
 <script type="text/javascript">
 $(document).ready(function() {
@@ -423,12 +415,11 @@ $(document).ready(function() {
 });
 </script>
 
- <script type="text/javascript">
+<script type="text/javascript">
 
     $(document).ready(function(){
         $('.view_data').click(function(){
             var itrID = $(this).attr("id");
-
             $.ajax({
                 url:"viewITRquery.php",
                 method:"post",
@@ -439,12 +430,9 @@ $(document).ready(function() {
 
                 }
             })
-
-
-            
         });
     });
 
-    </script>
+</script>
 </body>
 </html>
