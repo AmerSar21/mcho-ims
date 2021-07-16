2607ba5aa8b62dda1ed0838608345d6ed4478dd1
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 02:09:13 +0800

    update test5

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -1,9 +1,4 @@
 <?php 
-	header('Access-Control-Allow-Origin: *');
-	header("Access-Control-Allow-Credentials: true");
-	header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-	header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-	header("Content-Type: application/json; charset=utf-8");
 
 	define('DB_NAME', 'id16200133_mappcho');
 	define('DB_USER', 'id16200133_mappcho2021');
@@ -15,7 +10,7 @@
 	if($mysqli->connect_error){
 		die("Connection Failed " . $mysqli->connect_error);
 	}else{
-		//echo "Connected."
+		echo "Connected Successfuly";
 	}
 
 ?>
