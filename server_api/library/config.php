2f78f275420fc918b7357167100571929017f6ac
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 03:03:06 +0800

    update 2

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -3,7 +3,7 @@
 	define('DB_NAME', 'id16200133_mappcho');
 	define('DB_USER', 'id16200133_mappcho2021');
 	define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
-	define('DB_HOST', 'databases.000webhost.com');
+	define('DB_HOST', '000webhost.com');
 
 	// define('DB_NAME', 'qOK07O7HjJ');
 	// define('DB_USER', 'qOK07O7HjJ');
