dc9188fceb8e497e1687b9a593f97395986fbdfb
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 02:01:03 +0800

    update test3

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -3,7 +3,7 @@
 	define('DB_NAME', 'id16200133_mappcho');
 	define('DB_USER', 'id16200133_mappcho2021');
 	define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
-	define('DB_HOST', 'databases.000webhost.com');
+	define('DB_HOST', '000webhost.com');
 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
