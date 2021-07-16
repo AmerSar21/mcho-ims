934419e25bdb4cb981c29c73f563b4fdc95939ce
Amer Hussien Sarangani <74028727+AmerSar21@users.noreply.github.com>
2021-07-17 01:30:50 +0800

    new server config address from 000webhost

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -14,19 +14,4 @@
 	}
 
 ?>
-<!-- <?php 
 
-	define('DB_NAME', 'qOK07O7HjJ');
-	define('DB_USER', 'qOK07O7HjJ');
-	define('DB_PASSWORD', 'QLEyhT5DK8');
-	define('DB_HOST', 'remotemysql.com');
-
-	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-	if($mysqli->connect_error){
-		die("Connection Failed " . $mysqli->connect_error);
-	}else{
-		//echo "Connected."
-	}
-
-?> -->
