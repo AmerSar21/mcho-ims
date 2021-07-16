b0c8fe3fb94311386ba9b1f83634cfbb615d3cd5
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 02:56:20 +0800

    revert to old test

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -1,9 +1,14 @@
 <?php 
 
-	define('DB_NAME', 'id16200133_mappcho');
-	define('DB_USER', 'id16200133_mappcho2021');
-	define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
-	define('DB_HOST', '000webhost.com');
+	// define('DB_NAME', 'id16200133_mappcho');
+	// define('DB_USER', 'id16200133_mappcho2021');
+	// define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
+	// define('DB_HOST', '000webhost.com');
+
+	define('DB_NAME', 'qOK07O7HjJ');
+	define('DB_USER', 'qOK07O7HjJ');
+	define('DB_PASSWORD', 'QLEyhT5DK8');
+	define('DB_HOST', 'remotemysql.com');
 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
--- server_api/proses-api.php
+++ server_api/proses-api.php
@@ -1,10 +1,10 @@
 <?php 
 
-	// header('Access-Control-Allow-Origin: *');
-	// header("Access-Control-Allow-Credentials: true");
-	// header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-	// header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-	// header("Content-Type: application/json; charset=utf-8");
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Credentials: true");
+	header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+	header("Content-Type: application/json; charset=utf-8");
 
 	include "library/config.php";
 
