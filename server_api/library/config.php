d2df894953e395f80cca2e7baa4aa4734aa9a8b0
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 02:06:14 +0800

    update test4

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -1,4 +1,9 @@
 <?php 
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Credentials: true");
+	header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+	header("Content-Type: application/json; charset=utf-8");
 
 	define('DB_NAME', 'id16200133_mappcho');
 	define('DB_USER', 'id16200133_mappcho2021');
