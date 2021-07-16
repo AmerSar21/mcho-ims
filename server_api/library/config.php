630fc6e6d768398f03ae2619871b6a18957257ce
AmerSar21 <amersarangani22@gmail.com>
2021-07-17 04:40:27 +0800

    revert

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -1,16 +1,16 @@
 <?php 
 
 	//000webhost.com server
-	define('DB_NAME', 'id16200133_mappcho');
-	define('DB_USER', 'id16200133_mappcho2021');
-	define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
-	define('DB_HOST', 'https://000webhost.com');
+	// define('DB_NAME', 'id16200133_mappcho');
+	// define('DB_USER', 'id16200133_mappcho2021');
+	// define('DB_PASSWORD', 'B{Kb+\sqqj?2E*Ky');
+	// define('DB_HOST', 'localhost');
 
 	//remotemysql.com server
-	// define('DB_NAME', 'qOK07O7HjJ');
-	// define('DB_USER', 'qOK07O7HjJ');
-	// define('DB_PASSWORD', 'QLEyhT5DK8');
-	// define('DB_HOST', 'remotemysql.com');
+	define('DB_NAME', 'qOK07O7HjJ');
+	define('DB_USER', 'qOK07O7HjJ');
+	define('DB_PASSWORD', 'QLEyhT5DK8');
+	define('DB_HOST', 'remotemysql.com');
 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
